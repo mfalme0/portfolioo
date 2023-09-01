4934a8a77cb3b1a7eacdcdfe0d4f9739bc280388
mfalme0 <76815758+mfalme0@users.noreply.github.com>
2023-09-01 17:49:08 +0300

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -22,7 +22,7 @@ function Projects() {
               imgPath={chatify}
               isBlog={false}
               title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              description="I am currently developing an overclocking and suite for laptops with both Intel and AMD CPUs for in order to make the unleash their full potential and also unlock the power of both the iGPU and dGPUs for the systems"
               ghLink="https://github.com/mfalme0/PerfSuite"
               demoLink="https://github.com/mfalme0/PerfSuite/releases/download/overclock/PerfSuite.7z"
             />
