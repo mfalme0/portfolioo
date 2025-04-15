5d7fc1a911d903f33186a2b26b50a278cb7d7505
Mfalme <76815758+mfalme0@users.noreply.github.com>
2025-04-15 09:25:20 +0300

    nanoid updates

--- app/components/Mboka.tsx
+++ app/components/Mboka.tsx
@@ -11,7 +11,7 @@ const experiences = [
     company: 'SteadFast Academy',
     role: 'Software Engineer',
     duration: 'April 2025 - Present',
-    description: 'I work with the institution to create run and maintain various tools for use by various internal department',
+    description: 'I work with the institution to create, run and maintain various tools for use by various internal department',
     icon: <GrCloudComputer />,
     color: 'blue'
   },
